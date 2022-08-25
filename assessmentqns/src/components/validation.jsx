7c0aa62341d32b89480d81d2cfe1b8fd63b6cc26
Monly081 <101558585+Monly081@users.noreply.github.com>
2022-08-26 05:55:45 +0800

    Validation Component

--- assessmentqns/src/components/validation.jsx
+++ assessmentqns/src/components/validation.jsx
@@ -1,93 +1,72 @@
 import React, { Component } from 'react';
+import { useState, useEffect } from 'react';
+import { FormErrors } from './FormErrors';
 
 class UENValidation extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { uen: '', errors: {},  };
-
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleValidation = this.handleValidation.bind(this);
+        this.state = {
+            uen: '',
+            formErrors: { uen: '' },
+            uenValid: false,
+            formValid: false,
+        }
     }
 
-    // handleChange(event) {
-    //     this.setState({ value: event.target.value });
-    // }
-
-    // handleSubmit(event) {
-    //     if (this.handleValidation()){
-    //         alert('UEN submitted as: ' + this.state.value);
-    //     } else{
-    //         alert('Form has errors');
-
-    //     }
-
-    //     event.preventDefault();
-    // }
-
-    // handleValidation(){
-    //     let uen = this.state.value;
-    //     let errors = {};
-    //     let formIsValid = true;
-
-    //     if (!this.state.value){
-    //         formIsValid = false;
-    //         errors['uen'] = 'Cannot be empty!';
-    //     }
-    // }
-
-    // const initialValues = { uen: '' };
-    // const [formValues, setFormValues] = useState(initialValues);
-    // const [formErrors, setFormErrors] = useState({});
-    // const [isSubmit, setIsSubmit] = useState(false);
-
-    handleChange = (e) => {
-        console.log(formValues);
-        const { uen, value } = e.target;
-        setFormValues({ ...formValues, uen: value });
-    };
-
-    handleSubmit = (e) => {
-        e.preventDefault();
-        setFormErrors(handleValidation(formValues));
-        setIsSubmit(true);
-    };
-
-
-    handleValidation = (values) => {
-        const errors = {};
-        const regex = /((S|T)([\d]{2})([A-Z]{2})([\d]{4})([A-Z])|(\d{9})([A-Z]))/g; //Enter UEN REGEX here
-        if (!values.uen) {
-            errors.uen = 'UEN is required!'
-        }
-
-        else if (!regex.test(values.uen)) {
-            errors.uen = 'Please ensure UEN is entered correctly!'
-        }
-
-        return errors;
+    handleUserInput = (e) => {
+        const name = e.target.name
+        const value = e.target.value;
+        console.log(name, value)
+        this.setState({ [name]: value },
+            () => { this.validateField(name, value) });
     }
 
+    validateField(fieldName, value) {
+        let fieldValidationErrors = this.state.formErrors;
+        let uenValid = this.state.uenValid;
+
+        switch (fieldName) {
+            case 'uen':
+                uenValid = value.toUpperCase().match(/((S|T)([\d]{2})([A-Z]{2})([\d]{4})([A-Z])|(\d{9})([A-Z]))/g);
+                fieldValidationErrors.uen = uenValid ? '' : ' Please enter correct syntax!';
+                break;
+
+            default:
+                break;
+        }
+
+        this.setState({ formErrors: fieldValidationErrors, uenValid: uenValid }, this.validateForm);
+
+    }
+
+    validateForm() {
+        this.setState({ formValid: this.state.uenValid });
+    }
+
+    errorClass(error) {
+        return (error.length === 0 ? '' : 'has-error');
+    }
 
     render() {
-        <div className='container'>
-            {/* <pre>{JSON.stringify(formValues, undefined, 2)}</pre> */}
-            <form onSubmit={handleSubmit}>
+        return (
+            <div className='validationform'>
                 <h1>UEN Validation</h1>
-                <div className='ui divider'></div>
-                <div className='ui form'>
-                    <div className='field'>
-                        <label>UEN: </label>
-                        <input type='text' value={formValues.uen} onChange={handleChange} />
-                        <p>{formErrors.uen}</p>
-                        <input type='Submit' />
+                <form className='UENValidate'>
+                    <FormErrors formErrors={this.state.formErrors} />
+                    <div>
+                        <label>UEN:</label>
+                        <input type='text'
+                            name='uen'
+                            className='form-control'
+                            value={this.state.uen}
+                            onChange={this.handleUserInput} />
 
+                        <button type='submit' disabled={!this.state.formValid}>Check</button>
                     </div>
-
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
+        )
     }
 }
 
